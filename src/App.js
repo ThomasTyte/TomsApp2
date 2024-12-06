550ccfff00b71c27f81beff3c933605b67153013
thomas <tom_tyte@hotmail.com>
2024-12-05 20:18:45 -0500

    update homepage

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
          <BrowserRouter>
          <Navbar />
          <Routes>
+             <Route path="/" element={<Home />} />
              <Route path="/home" element={<Home />} />
              <Route path="/contact" element={<Contact />} />
          </Routes>
